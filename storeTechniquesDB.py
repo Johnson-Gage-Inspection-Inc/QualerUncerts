bb7aea01d14df85895b8e95f9b9a89bca8bcd07e
Jeff Hall <rhythmatician5@gmail.com>
2025-02-27 19:16:04 -0600

    Add script to write CSV data to SQL database using pandas and SQLAlchemy

---
+++ storeTechniquesDB.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://postgres:postgres@192.168.1.177:5432/qualer')
+
+
+def write_csv_to_sql(engine, tableName, path):
+    with open(path, encoding='utf-8') as f:
+        pd.read_csv(f).to_sql(tableName, engine, if_exists='replace', index=False)
+
+
+write_csv_to_sql(engine, 'techniques', 'C:/Users/JGI/Jeff H/Escape Dantes Inferno/QualerUncerts/csv/TechniquesList.csv')
+write_csv_to_sql(engine, 'service_capabilities', 'C:/Users/JGI/Jeff H/Escape Dantes Inferno/QualerUncerts/csv/ServiceCapabilities.csv')
+
+print("Done.")
