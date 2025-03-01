06016d74a767ced0f29d046973cbcf3af5be9123
Jeff Hall <rhythmatician5@gmail.com>
2025-02-28 20:41:51 -0600

    Add script to fetch certification capabilities from Qualer and save to JSON

---
+++ cmcs.sh
@@ -0,0 +1,17 @@
+curl ^"https://jgiquality.qualer.com/CertificationCapability/Capabilities_Read?siteId=96552^" ^
+  -H ^"accept: */*^" ^
+  -H ^"accept-language: en-US,en;q=0.9^" ^
+  -H ^"cache-control: no-cache, must-revalidate^" ^
+  -H ^"clientrequesttime: 2025-02-28T20:05:45^" ^
+  -b ^"cf_52662_id=8feb6fae-08a8-4031-b248-5c3beae42dee; _fuid=OWQzZGNjZWMtYTQ0YS00ZmE1LWFhNjEtNTdjY2NkZjJmZGEy; __utmz=147229740.1712691456.1.1.utmcsr=(direct)^|utmccn=(direct)^|utmcmd=(none); MFS0DK=0:::2; MFS0L9X=25574900-bed2-4f25-abe9-7b7083035441:::2; ajs_anonymous_id=98eaa4c6-dc04-4beb-bd79-ad1ccc81e8b3; _gcl_au=1.1.828456653.1737671505; mp_bfb01c59d1531c4cf9cd86439423aa91_mixpanel=^%^7B^%^22distinct_id^%^22^%^3A^%^20^%^22^%^24device^%^3A194954af6a6667-0a446c1984141-26011851-13c680-194954af6a6667^%^22^%^2C^%^22^%^24device_id^%^22^%^3A^%^20^%^22194954af6a6667-0a446c1984141-26011851-13c680-194954af6a6667^%^22^%^2C^%^22^%^24initial_referrer^%^22^%^3A^%^20^%^22^%^24direct^%^22^%^2C^%^22^%^24initial_referring_domain^%^22^%^3A^%^20^%^22^%^24direct^%^22^%^2C^%^22__mps^%^22^%^3A^%^20^%^7B^%^7D^%^2C^%^22__mpso^%^22^%^3A^%^20^%^7B^%^22^%^24initial_referrer^%^22^%^3A^%^20^%^22^%^24direct^%^22^%^2C^%^22^%^24initial_referring_domain^%^22^%^3A^%^20^%^22^%^24direct^%^22^%^7D^%^2C^%^22__mpus^%^22^%^3A^%^20^%^7B^%^7D^%^2C^%^22__mpa^%^22^%^3A^%^20^%^7B^%^7D^%^2C^%^22__mpu^%^22^%^3A^%^20^%^7B^%^7D^%^2C^%^22__mpr^%^22^%^3A^%^20^%^5B^%^5D^%^2C^%^22__mpap^%^22^%^3A^%^20^%^5B^%^5D^%^7D; _ga=GA1.2.170579144.1737671505; _ga_MQDQ7SCNEK=GS1.1.1738115170.2.0.1738115201.29.0.2017288033; GUID=^\^"f401a17e-3d37-4545-bb33-7da5153a7827^\^"; Qualer.Employee.Login.SessionId=c6ee3cf739fa4b09be6388387431d791; __RequestVerificationToken_L3NoYXJlZC1zZWN1cmVk0=PH-BVluNtuVRhmVZssLE-mDr10deLCQBKU44IlE-5_YLd0HlTZYXv5VLyDt4q_X_T1Z-g7ltFNlS9q63nGcNWi5l5dk1; __utmc=147229740; Qualer.auth=7347223A9574604CEBF6195B10B07563DF5E79361695A56F32684C71F85EB1EB57CA796EA93769D8DC66A92405E02D9A9E0C88A93370F9D50B9E6058BBC0B7AB12F667962CCD8EF713CA1A7C27E8AFF74701399993AB8158CFC37787AB2DF7110F47A545780D503DF56089F24E11920C9E7BCD08F594E5C5AEE3F415BF531501990606EB59883DE732FE5FDBC0D3540CB42483FE; __utma=147229740.1552134142.1712691456.1740788091.1740793529.63; ASP.NET_SessionId=1ldsopukbamzj3dozq4lc0vk; __utmb=147229740.17.10.1740793529; RT=^\^"z=1^&dm=qualer.com^&si=af85b7fd-c8e6-4bf3-be4c-5dda7bdadc29^&ss=m7pjiqj3^&sl=3c^&tt=1ofo^&bcn=https^%^3A^%^2F^%^2Fmetrics.qualer.com^%^2Fapi^%^2Fmetrics^\^"^" ^
+  -H ^"pragma: no-cache^" ^
+  -H ^"priority: u=1, i^" ^
+  -H ^"referer: https://jgiquality.qualer.com/ServiceGroupTechnique/Technique?ServiceGroupId=14319^" ^
+  -H ^"sec-ch-ua: ^\^"Not(A:Brand^\^";v=^\^"99^\^", ^\^"Google Chrome^\^";v=^\^"133^\^", ^\^"Chromium^\^";v=^\^"133^\^"^" ^
+  -H ^"sec-ch-ua-mobile: ?0^" ^
+  -H ^"sec-ch-ua-platform: ^\^"Windows^\^"^" ^
+  -H ^"sec-fetch-dest: empty^" ^
+  -H ^"sec-fetch-mode: cors^" ^
+  -H ^"sec-fetch-site: same-origin^" ^
+  -H ^"user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36^" ^
+  -H ^"x-requested-with: XMLHttpRequest^" > json/certificationCapabilities.json
